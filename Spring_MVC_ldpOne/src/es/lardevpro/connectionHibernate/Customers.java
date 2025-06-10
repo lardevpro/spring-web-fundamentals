8aab9ff4bcbdce467759648b93165c97ada48cd1
lardevpro <lardevpro@gmail.com>
2025-06-10 20:19:05 +0200

    Refactor Hibernate Configuration and Update Customer Entity
    - Added @GeneratedValue annotation to the id field in the Customers entity to enable auto-incrementing.
    - Introduced a new hibernateView method in UserController for testing Hibernate connection and saving a new Customer.
    - Updated the database connection URL in hibernate.cfg.xml from 'panaderia' to 'bakery'.
    - Cleaned up unnecessary comments and improved code formatting for better readability.

--- Spring_MVC_ldpOne/src/es/lardevpro/connectionHibernate/Customers.java
+++ Spring_MVC_ldpOne/src/es/lardevpro/connectionHibernate/Customers.java
@@ -2,6 +2,8 @@ package es.lardevpro.connectionHibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -24,6 +26,7 @@ public class Customers {
 
 	//Attr
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private String id;
 	
--- Spring_MVC_ldpOne/src/es/lardevpro/spring/mvc/UserController.java
+++ Spring_MVC_ldpOne/src/es/lardevpro/spring/mvc/UserController.java
@@ -1,6 +1,10 @@
 package es.lardevpro.spring.mvc;
 
 import javax.validation.Valid;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import es.lardevpro.connectionHibernate.Customers;
+
 
 
 
@@ -17,28 +23,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-	//para validar espacios en blanco
+
 	@InitBinder
 	public void myBinder(WebDataBinder binder) {
 		
 		StringTrimmerEditor trim = new StringTrimmerEditor(true);
-		
-		binder.registerCustomEditor(String.class, trim);
-		
+		binder.registerCustomEditor(String.class, trim);	
 	}
 	
 	
 	@RequestMapping("/viewFormRegister")
 	public String userRegistration(Model model) {
 		
-		
-		//usuario
-		model.addAttribute("currentUser",new User()); //Model añade como atributo del página 
-		
-		//sexo
+		model.addAttribute("currentUser",new User()); 
 		model.addAttribute("sexOptions", Sex.getSexOptions());
-	    
-	    //país
 	    model.addAttribute("countryOptions", CountryOptions.getCountryOptions());
 
 		
@@ -46,20 +44,42 @@ public class UserController {
 		
 	}
 	
-	//@Valid dice al sistema que está usando validación
-	//@ModelAttribute inyecta el usuario actual
-	//BindigResult 
+	
 	@RequestMapping("/userConfirmation")
 	public String processForm(@Valid @ModelAttribute("currentUser") User user, BindingResult resVal, Model model) {
 		
 		
 		if(resVal.hasErrors()) {
 				model.addAttribute("sexOptions", Sex.getSexOptions());
-				model.addAttribute("countryOptions", CountryOptions.getCountryOptions()); // Si tienes más opciones dinámicas
-		     return "formUserRegister"; // Vuelve al formulario con errores
+				model.addAttribute("countryOptions", CountryOptions.getCountryOptions()); 
+		     return "formUserRegister";
 		} else {
-			return "userRegistrationConfirmation";//si no hay error va a la página de confirmación
+			return "userRegistrationConfirmation";
+		}
+	}	
+	
+	@RequestMapping("/hibernateView")
+	public String hibernateConnection() {
+		
+		SessionFactory sf = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customers.class).buildSessionFactory();
+		
+		Session session = sf.openSession();
+		
+		try {
+			
+			Customers client = new Customers("Juan", "Lara", "España");
+			
+			session.beginTransaction();
+			session.save(client);
+			session.getTransaction().commit();
+			
+			
+		} finally {
+			
+			sf.close();
+			
 		}
 		
-	}	
+		return "hibernateConnection";
+	}
 }
